a37dca9eea30f57dd7837db2b22a57432bad7a6f
aranscope <aran.c.long@gmail.com>
2020-01-26 14:07:29 +0000

    Fix temp ip

--- service.central-heating/controllers/temperature_controller.go
+++ service.central-heating/controllers/temperature_controller.go
@@ -87,7 +87,7 @@ func setRadiatorState(state string) error {
 		return err
 	}
 
-	req, err := http.NewRequest(http.MethodPatch, "http://service.tplink-smart-plug:8082/plug/192.168.1.119", bytes.NewReader(js))
+	req, err := http.NewRequest(http.MethodPatch, "http://service.tplink-smart-plug:8082/plug/192.168.1.102", bytes.NewReader(js))
 	if err != nil {
 		return err
 	}
